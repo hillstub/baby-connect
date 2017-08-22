b0aa886a9c4cc1fcd9a70af57e81da8b9bb3e392
H.M. van Rossum <hmvanrossum@MacBook-Air-van-HM.local>
2017-08-21 17:57:15 -0700

    added unknown_child_error.js

---
+++ src/unknown_child_error.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 Marshall Roch <marshall@mroch.com>
+// All rights reserved.
+
+"use strict";
+
+class UnknownChildError {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+module.exports = UnknownChildError;
